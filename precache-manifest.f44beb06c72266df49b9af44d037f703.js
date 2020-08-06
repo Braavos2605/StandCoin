5e87b5a0979f709766febe55a6b46cd51470389c
Braavos2605 <56270193+Braavos2605@users.noreply.github.com>
2020-08-07 00:00:46 +0300

    Updates

---
+++ precache-manifest.f44beb06c72266df49b9af44d037f703.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d939177988c87ed30b2f5e366d696ee",
+    "url": "/index.html"
+  },
+  {
+    "revision": "caca5c71645a471bdc48",
+    "url": "/static/css/2.86df01db.chunk.css"
+  },
+  {
+    "revision": "993bd38e203ec5555a9c",
+    "url": "/static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "caca5c71645a471bdc48",
+    "url": "/static/js/2.e12f878f.chunk.js"
+  },
+  {
+    "revision": "21ced859ea2b2d6b856d461ad6c2afed",
+    "url": "/static/js/2.e12f878f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "993bd38e203ec5555a9c",
+    "url": "/static/js/main.b17bee6a.chunk.js"
+  },
+  {
+    "revision": "14ca0e10e9322a566f9c",
+    "url": "/static/js/runtime-main.32471253.js"
+  },
+  {
+    "revision": "1fef9de33013e1c26954457113dbad19",
+    "url": "/static/media/test.1fef9de3.jpg"
+  }
+]);
